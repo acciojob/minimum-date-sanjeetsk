721eefbe4319dc7fd0299c1f56f846e339129a0d
Sanjeet Kumar <96577772+sanjeetsk@users.noreply.github.com>
2024-12-05 04:23:57 +0530

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function minDate(dates) {
   //write you code here
+  const earliestDate = new Date(
+    Math.min(...dates.map(date => new Date(date)))
+  )
+
+  const year = earliestDate.getFullYear();
+  const month = String(earliestDate.getMonth() + 1).padStart(2, "0");
+  const day = String(earliestDate.getDate()).padStart(2, "0");
+
+  return `${year}/${month}/${day}`;
 }
 
 // Do not change the code
